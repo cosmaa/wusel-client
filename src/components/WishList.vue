4d6315229c60cc5c120e14884d511845afe06d64
ConstanzeMaass2610 <constanze.maass@otto.de>
2020-07-08 12:05:45 +0200

    [Refaktoring] add texts json

--- src/components/WishList.vue
+++ src/components/WishList.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="container" style="text-align: center">
-      <button class="language_button" @click="change_language()">{{ value }}</button>
-      <p>
-    <span v-html="text" id="text_wish_list" class="description_text_wish_list">
-      {{ text }}
-    </span>
-      </p>
+    <h4 class="wusel_h4">WISH-LIST</h4>
+    <button class="language_button" @click="change_language()">{{ value }}</button>
+    <p>
+      <span id="text_wish_list" class="description_text_wish_list" v-html="text">
+        {{ text }}
+      </span>
+    </p>
 
     <div class="wish_list_container">
-      <Wish v-for="(wish, index) in myJson" :key="index" v-bind="{ wish: wish.wish }" />
+      <Wish v-for="(wish, index) in wishes_json" :key="index" v-bind="{ wish: wish.wish }" />
     </div>
   </div>
 </template>
 
 <script>
-import json from '../../data/wishes.json'
+import wishes_json from '../../data/wishes.json'
+import texts_json from '../../data/texts.json'
 import Wish from './includes/Wish'
 
 export default {
@@ -23,16 +25,11 @@ export default {
   data() {
     return {
       value: 'de?',
-      text: '',
-      en:
-        "This is a list of tasks and projects that I (and Renate) are working on. <span class='rainbow'>Wusel.Space</span>  is in English as it is an ongoing process of learning to speak and write English. Soon there will be an area where you can tell me if I have misspelled something.",
-      de:
-        "Das ist eine Liste von Aufgaben und Projekten an denen ich (und Renate) arbeiten. <span class='rainbow'>Wusel.Space</span> ist auf Englisch, da es ein fortlaufender Prozesse ist english reden und schreiben zu lernen. Bald gibts auch einen Bereich, in dem ihr mir mitteilen könnt wenn ich etwas falsch geschrieben habe.",
-      myJson: json
+      text: texts_json.en,
+      en: texts_json.en,
+      de: texts_json.de,
+      wishes_json: wishes_json
     }
-  },
-  created() {
-    this.text = this.en
   },
   methods: {
     change_language() {
